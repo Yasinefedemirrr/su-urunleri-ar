5ae6bcc3786f946abc02364b8132ce46c249c81b
Yasin Efe Demir <yasinefedemir9@gmail.com>
2026-05-13 00:34:02 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const startBtn = document.getElementById("startBtn");
+const startScreen = document.getElementById("startScreen");
+
+startBtn.addEventListener("click", () => {
+  startScreen.style.display = "none";
+});
