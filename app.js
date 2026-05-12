e45709882577929e4bc502a29402097473cb2709
Emre Özer <eozer1371@gmail.com>
2026-05-13 01:26:56 +0300

    deneme

--- app.js
+++ app.js
@@ -1,42 +1,123 @@
+/* ELEMENTLER */
+
 const popup = document.getElementById("fishPopup");
+
+const fishImage = document.getElementById("fishImage");
+
+const fishName = document.getElementById("fishName");
+
+const fishDesc = document.getElementById("fishDesc");
+
+const fishSize = document.getElementById("fishSize");
+
+const fishHabitat = document.getElementById("fishHabitat");
+
 const closeBtn = document.getElementById("closeBtn");
 
 const startBtn = document.getElementById("startBtn");
+
 const startScreen = document.getElementById("startScreen");
 
-/* Başlat butonu */
+/* BALIK DATASI */
+
+const fishData = {
+
+  0: {
+
+    name: "Levrek",
+
+    desc: "Levrek Ege Denizi’nde yaşayan etçil bir balıktır.",
+
+    habitat: "Ege Denizi",
+
+    size: "40 cm",
+
+    image: "./assets/images/levrek.jpg"
+  },
+
+  1: {
+
+    name: "Somon",
+
+    desc: "Somon göç eden güçlü bir balık türüdür.",
+
+    habitat: "Kuzey Atlantik",
+
+    size: "70 cm",
+
+    image: "./assets/images/somon.jpg"
+  },
+
+  2: {
+
+    name: "Palyaço Balığı",
+
+    desc: "Mercan resiflerinde yaşayan tropikal balıktır.",
+
+    habitat: "Pasifik Okyanusu",
+
+    size: "15 cm",
+
+    image: "./assets/images/clownfish.jpg"
+  }
+
+};
+
+/* BAŞLAT BUTONU */
 
 startBtn.addEventListener("click", () => {
+
   startScreen.style.display = "none";
+
 });
 
-/* Popup kapatma */
+/* POPUP KAPAT */
 
 closeBtn.addEventListener("click", () => {
+
   popup.classList.add("hidden");
+
 });
 
-/* MindAR target eventleri */
+/* TARGET EVENTLERİ */
 
 document.addEventListener("DOMContentLoaded", () => {
 
-  const target = document.querySelector("[mindar-image-target]");
+  const targets = document.querySelectorAll("[mindar-image-target]");
 
-  /* Görsel algılanınca */
+  targets.forEach((target, index) => {
 
-  target.addEventListener("targetFound", () => {
+    /* TARGET BULUNDU */
 
-    console.log("BALIK ALGILANDI");
+    target.addEventListener("targetFound", () => {
 
-    popup.classList.remove("hidden");
+      console.log("TARGET BULUNDU:", index);
 
-  });
+      const fish = fishData[index];
 
-  /* Görsel kaybolunca */
+      if (!fish) return;
 
-  target.addEventListener("targetLost", () => {
+      fishImage.src = fish.image;
 
-    console.log("BALIK KAYBOLDU");
+      fishName.innerText = fish.name;
+
+      fishDesc.innerText = fish.desc;
+
+      fishSize.innerText = fish.size;
+
+      fishHabitat.innerText = fish.habitat;
+
+      popup.classList.remove("hidden");
+
+    });
+
+    /* TARGET KAYBOLDU */
+
+    target.addEventListener("targetLost", () => {
+
+      console.log("TARGET KAYBOLDU:", index);
+
+    });
 
   });
 
