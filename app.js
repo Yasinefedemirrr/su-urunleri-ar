4c6688a865eaab2c1f4541dc0da11e3bdca5e77c
Yasin Efe Demir <yasinefedemir9@gmail.com>
2026-05-13 01:01:57 +0300

    pop-up sistemi

--- app.js
+++ app.js
@@ -1,6 +1,43 @@
+const popup = document.getElementById("fishPopup");
+const closeBtn = document.getElementById("closeBtn");
+
 const startBtn = document.getElementById("startBtn");
 const startScreen = document.getElementById("startScreen");
 
+/* Başlat butonu */
+
 startBtn.addEventListener("click", () => {
   startScreen.style.display = "none";
+});
+
+/* Popup kapatma */
+
+closeBtn.addEventListener("click", () => {
+  popup.classList.add("hidden");
+});
+
+/* MindAR target eventleri */
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  const target = document.querySelector("[mindar-image-target]");
+
+  /* Görsel algılanınca */
+
+  target.addEventListener("targetFound", () => {
+
+    console.log("BALIK ALGILANDI");
+
+    popup.classList.remove("hidden");
+
+  });
+
+  /* Görsel kaybolunca */
+
+  target.addEventListener("targetLost", () => {
+
+    console.log("BALIK KAYBOLDU");
+
+  });
+
 });
